15fa6605f5269f56582fca1c388df98e1649d814
MD RASHID ALI <rashid@ith.tech>
2022-06-16 10:33:16 +0530

    logger added and 404 handled

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 require('./config/index');
 require('./models/db');
 
+const logger = require('morgan');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
-
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
@@ -17,5 +18,26 @@ const todo = require('./routes/todo');
 
 app.use('/auth', auth);
 app.use('/todo', todo);
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals,
+    let errMessage = err.message;
+
+    // send the error reponse
+    res.status(err.status || 500);
+    res.send({
+        success: false,
+        message: errMessage,
+    });
+});
 
 module.exports = app;
